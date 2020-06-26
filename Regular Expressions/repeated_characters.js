4ec51e96e93804520bf8f96f17c0a4b22adae41e
uaaeu <yurtmusa@gmail.com>
2020-06-26 17:28:55 +0300

    match character that occur zero or more with *

--- Regular Expressions/repeated_characters.js
+++ Regular Expressions/repeated_characters.js
@@ -2,3 +2,12 @@
 let difficultSpelling = "Mississippi";
 let myRegex = /s+/g; // + character the letter s occurs one or more times
 let result = difficultSpelling.match(myRegex);
+
+//Match Characters that Occur Zero or More Times
+let soccerWord = "gooooooooal!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/; // * character to matches g and o if there is
+soccerWord.match(goRegex); // Returns ["goooooooo"]
+gPhrase.match(goRegex); // Returns ["g"]
+oPhrase.match(goRegex); // Returns null
