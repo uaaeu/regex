5aa6d7a640eaed63a3083b21fb4c433ae074913a
uaaeu <yurtmusa@gmail.com>
2020-06-27 15:26:34 +0300

    matching all whitespace in string

---
+++ Regular Expressions/match_whitespace.js
@@ -0,0 +1,4 @@
+//Matching Whitespaces
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace); //returns [ ' ', ' ', ' ', ' ', ' ' ]
