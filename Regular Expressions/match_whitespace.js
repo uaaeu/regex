8a2dcf9ea4f106fb8208c38e45c33277d21f44ed
uaaeu <yurtmusa@gmail.com>
2020-06-27 15:34:20 +0300

    matching NON-whitespace characters

--- Regular Expressions/match_whitespace.js
+++ Regular Expressions/match_whitespace.js
@@ -1,4 +1,10 @@
 //Matching Whitespaces
 let sample = "Whitespace is important in separating words";
-let countWhiteSpace = /\s/g; // Change this line
+let countWhiteSpace = /\s/g; //matches all the whitespaces with \s (lowercase)
 let result = sample.match(countWhiteSpace); //returns [ ' ', ' ', ' ', ' ', ' ' ]
+
+
+//Matching Non-Whitespace Characters
+let countNonWhiteSpace = /\S/g; //matches all the NON-whitespaces with \S (UPPERCASE)
+let resultNon = sample.match(countNonWhiteSpace); 
+//returns [ 'W', 'h', 'i', 't', 'e', 's', 'p', 'a', 'c', 'e', 'i', 's', 'i', 'm', 'p', 'o', 'r', 't', 'a', 'n', 't', 'i', 'n', 's', 'e', 'p', 'a', 'r', 'a', 't', 'i', 'n', 'g', 'w', 'o', 'r', 'd', 's' ]
