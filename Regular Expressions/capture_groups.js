d1f56eaa052e9bb239c6bb6675251e426e205194
uaaeu <yurtmusa@gmail.com>
2020-06-27 17:05:53 +0300

    using capture groups to search and replace

--- Regular Expressions/capture_groups.js
+++ Regular Expressions/capture_groups.js
@@ -2,3 +2,9 @@
 let repeatNum = "42 42 42";
 let reRegex = /^(\d+)\s\1\s\1$/; // define beggining and end line with ^ and $, (\d+) groups first digits, \1 test next digits with the first group
 let result = reRegex.test(repeatNum); //return true
+
+//Use Capture Groups to Search and Replace
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // three groups captured with id $1 $2 $3
+let replaceText = "$3 $2 $1"; // text to replace reverse
+let result = str.replace(fixRegex, replaceText); // return "three two one"
