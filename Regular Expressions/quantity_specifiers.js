9531f6268057c6ee33a4a27b9990ad4e523cfd77
uaaeu <yurtmusa@gmail.com>
2020-06-27 15:44:25 +0300

    specify only the lower number of matches

--- Regular Expressions/quantity_specifiers.js
+++ Regular Expressions/quantity_specifiers.js
@@ -1,4 +1,10 @@
 //Specify Upper and Lower Number of Matches
 let ohStr = "Ohhh no";
-let ohRegex = /Oh{3,6}\sno/i; // with h{3,6} regex only match with h repeated between 3 and 6
+let ohRegex = /oh{3,6}\sno/i; // with h{3,6} regex only match with h repeated between 3 and 6
 let result = ohRegex.test(ohStr); //returns true
+
+//Specify Only the Lower Number of Matches
+let haStr = "Hazzzzah";
+let haRegex = /haz{4,}ah/i; // with z{4,} regex matches minimum 4 times repeated z  
+let result = haRegex.test(haStr); //returns true
+
